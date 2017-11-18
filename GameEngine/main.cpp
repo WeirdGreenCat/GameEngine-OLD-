b0277cb39e8734e0e0d9b6d2d82aad01995e17c2
WeirdGreenCat <Keny.Ivanov@gmail.com>
2017-11-18 15:51:33 +0300

    start project

---
+++ GameEngine/main.cpp
@@ -0,0 +1,12 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+#include <iostream>
+#include "config.hpp"
+using namespace sf;
+
+int main(void) 
+{
+	RenderWindow window(VideoMode(1024, 768), "Game", sf::Style::Default, sf::ContextSettings(24));
+
+	return 0;
+}
