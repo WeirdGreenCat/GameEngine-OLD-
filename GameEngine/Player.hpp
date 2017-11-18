727d08450c4cb092bbac53ac0dda608b483a28cf
WeirdGreenCat <Keny.Ivanov@gmail.com>
2017-11-18 20:42:25 +0300

    Абстрактный класс персонажа

---
+++ GameEngine/Player.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+ class Player abstract
+{
+	public:
+	Player(float x,float y, float z,int hp) : x(x),y(y),z(z),hp(hp) {};
+
+	virtual void characterSize(float w, float h, float d)
+	{
+		this->w = w;
+		this->h = h;
+		this->d = d;
+	}
+	virtual ~Player() = 0
+	{
+
+	};
+	private:
+	float x, y, z; // начальная точка в которой находится персонаж
+	int hp, mana;
+	float w, h, d; //Размер персонажа 
+};
