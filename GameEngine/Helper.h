93f68f46384061387495d9fcdaafc1a3868dc9d0
WeirdGreenCat <Keny.Ivanov@gmail.com>
2017-11-19 16:24:18 +0300

    PMap

--- GameEngine/Helper.h
+++ GameEngine/Helper.h
@@ -1,2 +1,26 @@
 #include "includer.hpp"
+namespace help
+{
+	void help()
+	{
+		ofstream save;
+		save.open("pMap.pMap");
+		for (int x = 0; x < 256; x++)
+		{
+			for (int y = 0; y < 50; y++)
+			{
+				for (int z = 0; z < 256; z++)
+				{
+					if (y == 0)
+					{
+						save << "1";
+					}
+					else save << "0";
 
+
+				}
+			}
+		}
+		save.close();
+	}
+}
--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -6,21 +6,8 @@ using namespace sf;
 int main(void) 
 {
 	RenderWindow window(VideoMode(1024, 768), "Game", sf::Style::Default, sf::ContextSettings(24));
-	ofstream save;
-	save.open("pMap.pMap");
-	for (int x = 0; x < 256; x++)
-	{
-		for (int y = 0; y < 50; y++)
-		{
-			for (int z = 0; z < 256; z++)
-			{
-				
-				
-				save << "0";
-			}
-		}
-	}
-	save.close();
+	PhisicsMap *pMap = new PhisicsMap("pMap.pMap");
+
 	std::cin;
 	return 0;
 }
